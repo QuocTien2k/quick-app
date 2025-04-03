ade9a42ab3cfb3c7d76020446364ca7cddde740a
QuocTien2k <hadesdqt@gmail.com>
2025-04-03 16:40:40 +0700

    init client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="my-2 mx-2">
+        <h2 className="text-red-500">Xin chao React From Tailwind</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
