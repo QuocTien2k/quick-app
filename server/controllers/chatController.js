2e0a54a56baf468ecb437f83ad8636703b9e9ed3
QuocTien2k <hadesdqt@gmail.com>
2025-04-07 16:08:19 +0700

    update router clear unread message

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const protect = require("../middlewares/authMiddleware");
 const Chat = require("../models/chat");
+const message = require("../models/message");
 
 //Create new chat
 router.post("/create-new-chat", protect, async (req, res) => {
@@ -68,6 +69,44 @@ router.get("/get-all-chats", protect, async (req, res) => {
   } catch (error) {
     res.status(400).send({
       message: "Lấy danh sách chat thất bại!" + error.message,
+      success: false,
+    });
+  }
+});
+
+//
+router.post("/clear-unread-message", protect, async (req, res) => {
+  try {
+    const chatId = req.body.chatId;
+
+    //1. we want to update the unread message count in chat collection
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      return res.status(404).send({
+        message: "Không tìm thấy cuộc trò chuyện!",
+        success: false,
+      });
+    }
+
+    const updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      { unreadMessage: 0 },
+      { new: true }
+    )
+      .populate("members")
+      .populate("lastMessage");
+
+    //2. we want to update the read property to true in message collection
+    await message.updateMany({ chatId: chatId, read: false }, { read: true });
+
+    res.status(200).send({
+      message: "Đánh dấu tất cả tin nhắn là đã đọc thành công!",
+      success: true,
+      data: updatedChat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Xóa tin nhắn chưa đọc thất bại!" + error.message,
       success: false,
     });
   }
